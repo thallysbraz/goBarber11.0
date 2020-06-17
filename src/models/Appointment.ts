7c5ecb6acb406e64bb306668b4884cc2b315181d
thallys.braz <thallys.braz@firstdecision.com.br>
2020-06-16 21:37:28 -0300

    registro de usuários

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,4 +1,17 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+/* eslint-disable camelcase */
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+    ManyToOne,
+    JoinColumn,
+} from "typeorm";
+
+import User from "./User";
+
+// KISS - Keep It Simples & Stupid
 
 @Entity("appointments")
 class Appointment {
@@ -6,10 +19,21 @@ class Appointment {
     id: string;
 
     @Column()
-    provider: string;
+    provider_id: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: "provider_id" })
+    provider: User;
 
     @Column("time with time zone")
     date: Date;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    // eslint-disable-next-line camelcase
+    update_at: Date;
 }
 
 export default Appointment;
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import { startOfHour } from "date-fns";
 import { getCustomRepository } from "typeorm";
 
@@ -5,14 +6,14 @@ import Appointment from "../models/Appointment";
 import AppointmentsRepository from "../repositories/AppointmentsRepository";
 
 interface Request {
-    provider: string;
+    provider_id: string;
     date: Date;
 }
 
 // Dependency Inversion
 
 class CreateAppointmentService {
-    public async execute({ provider, date }: Request): Promise<Appointment> {
+    public async execute({ provider_id, date }: Request): Promise<Appointment> {
         const appointmentsRepository = getCustomRepository(
             AppointmentsRepository,
         );
@@ -27,7 +28,7 @@ class CreateAppointmentService {
         }
 
         const appointment = appointmentsRepository.create({
-            provider,
+            provider_id,
             date: appointmentDate,
         });
         await appointmentsRepository.save(appointment);
